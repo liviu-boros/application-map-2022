717878e9f9e9449eb01f327b6c55ec2a72e1d7c2
liviu-boros <boros.liviu@gmail.com>
2022-07-03 14:02:17 +0300

    Mobile fix and pretty code
    - fixed mobile size of info card
    - modified info card h2 and p to be dynamic font sizes on mobile
    - various cleaning up

--- js/jscripts.js
+++ js/jscripts.js
@@ -16,134 +16,67 @@ const navIconsList_sec2 = document.querySelectorAll(".navigation-icon.section-2"
 const totalImages_sec2 = imageList_sec2.length;
 var imageCounter_sec2 = 0;
 
-//next button click logic
+// !TODO abstract this logic
+//next button section-1 click logic
 nextBtn_sec1.addEventListener("click", () => {
-  imageList_sec1.forEach((slide) => {
-    slide.classList.remove("active");
+  imageList_sec1.forEach((image) => {
+    image.classList.remove("active");
   });
-  navIconsList_sec1.forEach((slideIcon) => {
-    slideIcon.classList.remove("active");
+  navIconsList_sec1.forEach((navIcon) => {
+    navIcon.classList.remove("active");
   });
-
   imageCounter_sec1++;
-
   if(imageCounter_sec1 > (totalImages_sec1 - 1)){
     imageCounter_sec1 = 0;
   }
-
   imageList_sec1[imageCounter_sec1].classList.add("active");
   navIconsList_sec1[imageCounter_sec1].classList.add("active");
 });
 
-//previous button click logic
+//previous button section-1 click logic
 prevBtn_sec1.addEventListener("click", () => {
-  imageList_sec1.forEach((slide) => {
-    slide.classList.remove("active");
+  imageList_sec1.forEach((image) => {
+    image.classList.remove("active");
   });
-  navIconsList_sec1.forEach((slideIcon) => {
-    slideIcon.classList.remove("active");
+  navIconsList_sec1.forEach((navIcon) => {
+    navIcon.classList.remove("active");
   });
-
   imageCounter_sec1--;
-
   if(imageCounter_sec1 < 0){
     imageCounter_sec1 = totalImages_sec1 - 1;
   }
-
   imageList_sec1[imageCounter_sec1].classList.add("active");
   navIconsList_sec1[imageCounter_sec1].classList.add("active");
 });
 
-//next button click logic
+//next button section-2 click logic
 nextBtn_sec2.addEventListener("click", () => {
-  imageList_sec2.forEach((slide) => {
-    slide.classList.remove("active");
+  imageList_sec2.forEach((image) => {
+    image.classList.remove("active");
   });
-  navIconsList_sec2.forEach((slideIcon) => {
-    slideIcon.classList.remove("active");
+  navIconsList_sec2.forEach((navIcon) => {
+    navIcon.classList.remove("active");
   });
-
   imageCounter_sec2++;
-
   if(imageCounter_sec2 > (totalImages_sec2 - 1)){
     imageCounter_sec2 = 0;
   }
-
   imageList_sec2[imageCounter_sec2].classList.add("active");
   navIconsList_sec2[imageCounter_sec2].classList.add("active");
 });
 
-//previous button click logic
+//previous button section-2 click logic
 prevBtn_sec2.addEventListener("click", () => {
-  imageList_sec2.forEach((slide) => {
-    slide.classList.remove("active");
+  imageList_sec2.forEach((image) => {
+    image.classList.remove("active");
   });
-  navIconsList_sec2.forEach((slideIcon) => {
-    slideIcon.classList.remove("active");
+  navIconsList_sec2.forEach((navIcon) => {
+    navIcon.classList.remove("active");
   });
-
   imageCounter_sec2--;
-
   if(imageCounter_sec2 < 0){
     imageCounter_sec2 = totalImages_sec2 - 1;
   }
-
   imageList_sec2[imageCounter_sec2].classList.add("active");
   navIconsList_sec2[imageCounter_sec2].classList.add("active");
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//image slider autoplay
-// var playSlider;
-
-// var repeater = () => {
-//   playSlider = setInterval(function(){
-//     slides.forEach((slide) => {
-//       slide.classList.remove("active");
-//     });
-//     slideIcons.forEach((slideIcon) => {
-//       slideIcon.classList.remove("active");
-//     });
-
-//     slideNumber++;
-
-//     if(slideNumber > (numberOfSlides - 1)){
-//       slideNumber = 0;
-//     }
-
-//     slides[slideNumber].classList.add("active");
-//     slideIcons[slideNumber].classList.add("active");
-//   }, 4000);
-// }
-// repeater();
-
-//stop the image slider autoplay on mouseover
-// slider.addEventListener("mouseover", () => {
-//   clearInterval(playSlider);
-// });
-
-// //start the image slider autoplay again on mouseout
-// slider.addEventListener("mouseout", () => {
-//   repeater();
-// });
-  
